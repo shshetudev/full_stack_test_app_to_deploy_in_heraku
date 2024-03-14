79a710bd774065a24280a5a0dd1bc0b38052bd54
shshetudev <shshetudev@gmail.com>
2024-03-14 06:27:33 +0600

    changing the port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080; // todo: change it to 8000
 
 app.use(cors());
 app.use(express.json());
